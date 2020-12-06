9c0493564cac37133fdfc5ad3c55ea54c7901275
Leif Park Jordan <lparkjordan@gmail.com>
2020-12-05 16:49:26 -0800

    Add basic monte carlo analysis. Fix applyAttenuators for the empty case

--- example/testScalingReceiverCount.m
+++ example/testScalingReceiverCount.m
@@ -10,74 +10,104 @@ mytime = tic;
 % Place a transmitter at one corner and a circle of recievers in the middle of the room
 % The transmitter performs a sector sweep while both receivers are
 % oriented towards the transmitter
+figure_num = 1;
+plot_impulse_responses = 0;
+plot_spatial_power = 0;
+plot_env = 0;
 
-room_size = [9, 9, 3];
-num_receivers = 16;
-circle_radius = 3;
-hpbw = 60;
 
-tx_pos = [-room_size(1)/2 * 0.9, room_size(2)/2 * 0.9];
 
-tx_set = sweepingTransceiver(tx_pos, 60, 64);
-rx_set = [];
-people = [];
-for i = 1:num_receivers
-    % add a randomly oriented person at the point specified
-    angle = (i-1)*2*pi/num_receivers;
-    pos = circle_radius*[cos(angle), sin(angle)];
-    person_width = 0.55;
-    human_permit = 10;
-    orientation = rand() * 2 * pi;
-    rotation = rand() * 2 * pi;
-    rx_pos = pos + person_width/2*sqrt(2)*[cos(orientation), sin(orientation)];
-    rx_set = [rx_set; sweepingTransceiver(rx_pos, hpbw, 1, angle2Points(rx_pos,tx_pos))];
-    
-    person = [pos, person_width, person_width, rotation, human_permit];
-    people = [people; person]; 
-end
+for sqrt_receivers = 1:7
+    num_receivers = sqrt_receivers^2;
+    repetitions = floor(250 / num_receivers);
+    max_powers = zeros(num_receivers,repetitions);
+    for rep = 1:repetitions
         
-% Create an obstacle
-obstacles = [0, 0, 0.5, 0.2, 0, 3.24];
+        room_size = [20, 20, 3];
+        person_width = 0.55;
+        circle_radius = 3;
 
-% Trace the channels
-[trace, tr_ccomps] = ch_trace( ...
-    tx_set, rx_set, room_size, ...
-    'max_refl', 4, ...
-    'obstacles', obstacles, ...
-    'attenuators', people);
+        usable_area = room_size(1:2) - 2 * person_width* sqrt(2) * ones(2,1);
+        uniform_spacing = usable_area / (sqrt(num_receivers));
+        grid_size = ceil(sqrt(num_receivers));
 
-toc(mytime);
+        hpbw = 60;
 
-%% Plot the received power for each receiver
-figure(1);
-clf
-set(gcf,'name','channel power in tx sector sweep');
-x	= trace.tx_set(1:64,4); 
-legend_txt = {};
-hold on
-for i = 1:num_receivers
-    plot(rad2deg(x), trace.power((i-1)*64+1:i*64));
-    legend_txt{end+1} = sprintf('rx%0d', i);
-end
-hold off
-legend(legend_txt);
-xlabel('tx direction');
-ylabel('channel power');
+        tx_pos = [-room_size(1)/2 * 0.9, room_size(2)/2 * 0.9];
+        tx_set = sweepingTransceiver(tx_pos, 60, 64);
 
-% Find the optimal sectors for serving the receivers
-figure_num = 2;
-for i = 1:num_receivers
-    [~, opt_dir] = max(trace.power((i-1)*64+1:i*64));
-    disp(['Optimal direction for rx1: ', num2str(rad2deg(tx_set(opt_dir,4))), ' degree']);
-    plotImpulseResponse(figure(figure_num), trace.impres((i-1)*64+opt_dir,:), 2.56e9);
+        rx_set = [];
+        people = [];
+        for i = 0:num_receivers-1
+            % add a randomly oriented person at the point specified
+            angle = (i-1)*2*pi/num_receivers;
+            %pos = circle_radius*[cos(angle), sin(angle)];
+            pos = [(mod(i,grid_size) - (grid_size-1)/2)*uniform_spacing(1), ...
+                   (floor(i/grid_size) - (grid_size-1)/2)*uniform_spacing(2)];
+            human_permit = 10;
+            orientation = rand() * 2 * pi;
+            rotation = rand() * 2 * pi;
+            rx_pos = pos + person_width/2*sqrt(2)*[cos(orientation), sin(orientation)];
+            rx_set = [rx_set; sweepingTransceiver(rx_pos, hpbw, 1, angle2Points(rx_pos,tx_pos))];
+
+            person = [pos, person_width, person_width, rotation, human_permit];
+            people = [people; person]; 
+        end
+
+        % Create an obstacle
+        obstacles = [];%[0, 0, 1.5, 1.5, 0, 3.24];
+
+        % Trace the channels
+        [trace, tr_ccomps] = ch_trace( ...
+            tx_set, rx_set, room_size, ...
+            'max_refl', 4, ...
+            'obstacles', obstacles, ...
+            'attenuators', people);
+
+        toc(mytime);
+
+        %% Plot the received power for each receiver
+        if (plot_spatial_power)
+            figure(figure_num);
+            clf
+            set(gcf,'name','channel power in tx sector sweep');
+            x	= trace.tx_set(1:64,4); 
+            legend_txt = {};
+            hold on
+            for i = 1:num_receivers
+                plot(rad2deg(x), trace.power((i-1)*64+1:i*64));
+                legend_txt{end+1} = sprintf('rx%0d', i);
+            end
+            hold off
+            legend(legend_txt);
+            xlabel('tx direction');
+            ylabel('channel power');
+            figure_num = figure_num + 1;
+        end
+        % Find the optimal sectors for serving the receivers
+        for i = 1:num_receivers
+            [max_powers(i,rep), opt_dir] = max(trace.power((i-1)*64+1:i*64));
+            disp(['Optimal direction for rx1: ', num2str(rad2deg(tx_set(opt_dir,4))), ' degree']);
+            if (plot_impulse_responses)
+                plotImpulseResponse(figure(figure_num), trace.impres((i-1)*64+opt_dir,:), 2.56e9);
+                figure_num = figure_num + 1;
+            end
+
+        end
+
+        % Plot the environment
+        if (plot_env)
+            figure(figure_num)
+            for i = 1:num_receivers
+                subplot(ceil(sqrt(num_receivers)),ceil(sqrt(num_receivers)),i)
+                plotEnvironment(figure(figure_num), trace, (i-1)*64+1);
+                title(sprintf('Rx %d', i))
+            end
+            figure_num = figure_num + 1;
+        end
+    end
+    figure(figure_num)
+    max_powers = reshape(max_powers, [], 1);
+    histogram(max_powers,20);
     figure_num = figure_num + 1;
 end
-
-% Plot the environment
-figure(figure_num)
-for i = 1:num_receivers
-    subplot(ceil(sqrt(num_receivers)),ceil(sqrt(num_receivers)),i)
-    plotEnvironment(figure(figure_num), trace, (i-1)*64+1);
-    title(sprintf('Rx %d', i))
-end
-figure_num = figure_num + 1;
--- functions/channel/applyAttenuators.m
+++ functions/channel/applyAttenuators.m
@@ -1,16 +1,24 @@
 function [ attenuations ] = applyAttenuators(paths, attenuators, hpbw)
 %APPLYATTENUATORS Summary of this function goes here
 %   Detailed explanation goes here
-
+    if isempty(attenuators)
+        attenuations = ones(size(paths,1),1);
+        return
+    end
     edges = objToEdges(attenuators); 
-
+    num_edges = size(edges,1);
+    edge_lengths = vecnorm(edges(:,1:2) - edges(:,3:4),2,2);
+    diagonal_lengths = sqrt(edge_lengths(1:4:end).^2 + edge_lengths(2:4:end).^2);
+    associated_diagonals = reshape(repmat(diagonal_lengths,4,1),[],1);
     num_paths = size(paths,1);
     num_segments = sum(~isnan(getPathReflectionObject(paths)),2) + 1;
-    intersect_counts = zeros(num_paths, 1);
+    total_fraction = zeros(num_paths, 1);
     attenuations = zeros(num_paths, 1);
+    
     for i = 1:num_paths
         % Number of segments is number of reflections + 1
-        path_intersects = [];
+        path_intersects = zeros(num_edges,2);
+        path_diagonals = repmat(associated_diagonals,num_segments(i),1);
         for j = 1:num_segments(i)
             if j == 1
                 segment = paths(i,1:4);
@@ -18,16 +26,23 @@ function [ attenuations ] = applyAttenuators(paths, attenuators, hpbw)
                 segment = [paths(i,(j-2)*5+3:(j-2)*5+4), paths(i,(j-1)*5+3:(j-1)*5+4)];
             end
             intersects = intersectEdges(segment, edges);
-            path_intersects = [path_intersects; intersects];
+            path_intersects((j-1)*num_edges+1:j*num_edges, :) = intersects;
         end
+        path_diagonals(any(isnan(path_intersects),2),:) = [];
+        path_diagonals = path_diagonals(1:2:end);
         path_intersects(any(isnan(path_intersects),2),:) = [];
         
-        % Calculate attenuation using analytical expression from NYUSIM
-        intersect_counts(i) = size(path_intersects,1)/2;
-        b = 9.8;
-        var = 0.31;
-        attenuations = intersect_counts .* (10*log10(b + pi/hpbw) + randn(size(attenuations))*var);
+        entry_points = path_intersects(1:2:end,:);
+        exit_points = path_intersects(2:2:end,:);
+        vectors = entry_points - exit_points;
+        distances = vecnorm(vectors,2,2);
+        diagonal_fractions = distances ./ path_diagonals;
+        total_fraction(i) = sum(diagonal_fractions);
+        
         
     end
-
+    % Calculate attenuation using analytical expression from NYUSIM
+    b = 9.8;
+    var = 0.31;
+    attenuations = total_fraction .* (10*log10(b + pi/hpbw) + randn(size(attenuations))*var);
 end
